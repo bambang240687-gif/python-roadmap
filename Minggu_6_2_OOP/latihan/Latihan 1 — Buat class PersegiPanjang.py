b69325d62888e146671c6d20ba5b000c05a3268c
Donto <bambang240687@gmail.com>
2025-12-03 20:49:02 +0700

    add folder Minggu_6_2_OOP

---
+++ Minggu_6_2_OOP/latihan/Latihan 1 — Buat class PersegiPanjang.py
@@ -0,0 +1,19 @@
+# Latihan 1 — Buat class PersegiPanjang (pythonic)
+class PersegiPanjang:
+    def __init__(self, panjang, lebar):
+        self.panjang = panjang
+        self.lebar = lebar
+    
+    def luas(self):
+        return self.panjang * self.lebar
+    
+    def keliling(self):
+        return 2 * (self.panjang + self.lebar)
+
+panjang = float(input("Masukkan panjang persegi panjang: "))
+lebar = float(input("Masukkan lebar persegi panjang: "))
+
+persegi_panjang = PersegiPanjang(panjang, lebar)
+
+print(f"Luas persegi panjang: {persegi_panjang.luas}")
+print(f"Keliling persegi panjang: {persegi_panjang.keliling}")
